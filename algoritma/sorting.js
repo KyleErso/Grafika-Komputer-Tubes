ee476e16c28e0ec36f3bd5694f3d9265b63ae272
evan <evankristian03@gmail.com>
2025-10-27 09:17:58 +0700

    Refractoring

--- algoritma/sorting.js
+++ algoritma/sorting.js
@@ -1,20 +1,22 @@
-export function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
-    const pivot = arr[arr.length - 1];
-    const left = [];
-    const right = [];
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
-    return [...quickSort(left), pivot, ...quickSort(right)];
-}
 
-// Example usage:
-let arr = [13, 72, 8, 9, 1, 5];
-console.log(quickSort(arr));
+export function quickSort(arr, callback, delay = 200) {
+  if (arr.length <= 1) return arr;
+
+  const pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) left.push(arr[i]);
+    else right.push(arr[i]);
+  }
+
+  if (callback) callback([...left, pivot, ...right]);
+  return new Promise(resolve => {
+    setTimeout(async () => {
+      const sortedLeft = await quickSort(left, callback, delay);
+      const sortedRight = await quickSort(right, callback, delay);
+      resolve([...sortedLeft, pivot, ...sortedRight]);
+    }, delay);
+  });
+}
